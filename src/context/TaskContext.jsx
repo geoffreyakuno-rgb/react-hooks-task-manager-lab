6ab0c9521b7f6e091eb2799d60fdfae3fe988120
Griffin <geoffre.akuno@student.moringaschool.com>
2026-02-26 16:24:57 +0300

    Task manager Lab

--- src/context/TaskContext.jsx
+++ src/context/TaskContext.jsx
@@ -1,6 +1,41 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
 
 export const TaskContext = createContext();
 
 export function TaskProvider({ children }) {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:6001/tasks")
+      .then((r) => r.json())
+      .then((data) => setTasks(data));
+  }, []);
+
+  function addTask(title) {
+    fetch("http://localhost:6001/tasks", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ title, completed: false }),
+    })
+      .then((r) => r.json())
+      .then((newTask) => setTasks([...tasks, newTask]));
+  }
+
+  function toggleComplete(task) {
+    fetch(`http://localhost:6001/tasks/${task.id}`, {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ completed: !task.completed }),
+    })
+      .then((r) => r.json())
+      .then((updated) =>
+        setTasks(tasks.map((t) => (t.id === updated.id ? updated : t)))
+      );
+  }
+
+  return (
+    <TaskContext.Provider value={{ tasks, addTask, toggleComplete }}>
+      {children}
+    </TaskContext.Provider>
+  );
 }
